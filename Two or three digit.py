445cc01dd0c7b68baad477b6bf6c5a235b9cf061
DISAN2010 <59792961+DISAN2010@users.noreply.github.com>
2020-02-12 21:25:15 +0530

    Update Two or three digit.py

--- Two or three digit.py
+++ Two or three digit.py
@@ -2,21 +2,21 @@
 Two digit or three digit number
 """
 
-def number1(num1):
-    if num1 <= 99:
-        print("Entered number is two digit : " , num1)
-    elif num1 <=999:
-        print("Entered number is three disgit : ", num1)
-    else:
-        print("Entered number is : ", num1)
-
 def number2(num2):
-    if num2 <= 99:
-        print("Entered number is two digit : " , num2)
-    elif num2 <=999:
-        print("Entered number is three disgit : ", num2)
+    if num2 > 9 and num2 < 100:
+        print("Entered number is two digit", num2)
+    elif num2 > 99 and num2 < 1000:
+        print("Entered number is three digit", num2)
     else:
         print("Entered number is : ", num2)
+
+def number1(num1):
+    if num1 > 9 and num1< 100:
+        print("Entered number is two digit", num1)
+    elif num1 >99 and num1 < 1000:
+        print("Entered number is three digit", num1)
+    else:
+        print("Entered number is : ", num1)
 
 def get_number():
     num1 = input("Enter a number : ")
